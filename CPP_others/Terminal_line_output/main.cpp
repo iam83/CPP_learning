d8ecbe4d46516e878093c6ec7ec0a18dfbdb6e72
Anton U <iam83iam@gmail.com>
2022-03-30 21:44:58 +0300

    update terminal output

--- CPP_others/Terminal_line_output/main.cpp
+++ CPP_others/Terminal_line_output/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 
-
-int main(){
-
-    system("clear");
-    while (1){
-    int x{80};
-    int y{24};
-
-    for (int i=y; i > 0; --i){
-        for (int j=x; j > 0; --j){
+void printLines(int width, int height, const char symbol){
+    for (int i=height; i > 0; --i){
+        for (int j=width; j > 0; --j){
             if (j==1 || j==80){
-                std::cout << "|";
-                continue;
+                std::cout << "0";
+            }else{
+                if (i == 24 || i == 1){
+                    std::cout << "0";
+                }else{
+                     std::cout << symbol;
+                    }
             }
-            std::cout << "-";
         }
 
         std::cout << std::endl;
     }
-    }
+}
 
+int main(){
+
+    system("clear");
+
+    int width {80};
+    int height {24};
+    const char symbol {'-'};
+    printLines(width, height, symbol);
+    
     return 0;
 }
