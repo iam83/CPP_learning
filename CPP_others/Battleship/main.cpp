7992643eeb922c203f5d9c06a3c8a90560033e0f
Anton U <anton.urkin@gmail.com>
2022-09-08 22:03:17 +0300

    battleship update

--- CPP_others/Battleship/main.cpp
+++ CPP_others/Battleship/main.cpp
@@ -40,76 +40,69 @@ int getRandomNumber(int min, int max){
         return static_cast<int>(rand() * fraction * (max - min + 1) + min);
      }
 
+void createField(std::array<std::array<int, 12>, 12> &field){
+    field.fill({0,0});
+}
+
 void createField(std::array<std::array<int, 10>, 10> &field){
     field.fill({0,0});
 }
 
-void checkFieldNeigh2(std::array<std::array<int, 10>, 10> &field){
+void printField2(std::array<std::array<int, 12>, 12> const &field){
+    
+    #ifdef _WIN32
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    #endif
 
-    for (int row = 0; row < 9; ++row){
-        for (int col = 0; col < 9; ++col){
-
-            if (field.at(row).at(col) == 0){
-                if (row >=1 && row <= 8 && col >= 1 && col <= 8){
-                    if (field.at(row-1).at(col) == 1 || field.at(row+1).at(col) == 1 || field.at(row).at(col-1) == 1
-                    || field.at(row).at(col+1) == 1 || field.at(row-1).at(col-1) == 1 || field.at(row+1).at(col+1) == 1
-                    || field.at(row+1).at(col-1) == 1 || field.at(row-1).at(col+1) == 1)
-                        field.at(row).at(col) = 8;
-                }
-                if (col == 0){
-                    if (field.at(row-1).at(col+1) == 1)
-                        field.at(row).at(col) = 8;
-                }
-
-                if (row == 0){
-                    if (field.at(row+1).at(col-1) == 1 || field.at(row).at(col-1) == 1)
-                        field.at(row).at(col) = 8;
-                }
-                if(row == 0 || col == 0)
-                    if (field.at(row).at(col+1) == 1 || field.at(row+1).at(col) == 1 || field.at(row+1).at(col+1) == 1)
-                        field.at(row).at(col) = 8;
-                if(row == 9 || col == 9)
-                    if (field.at(row).at(col-1) == 1 || field.at(row-1).at(col) == 1 || field.at(row-1).at(col-1) == 1)
-                        field.at(row).at(col) = 8;
-            }
-                
-        }
+    std::string letters = "ABCDEFGHKL";
+    std::cout << "   ";
+    for (auto const &lett : letters){
+        std::cout << lett << " ";
     }
-}
+    std::cout << std::endl;
 
-void checkFieldNeigh(std::array<std::array<int, 10>, 10> &field){
-
-    for (int row = 0; row < field.size(); ++row){
-        for (int col = 0; col < field.size(); ++col){
-
-            if (field.at(row).at(col) == 0){
-                if (row > 0 && row <= 8 && col > 0 && col <= 8){
-                    if (field[row-1][col] == 1 || field[row+1][col] == 1 || field[row][col-1] == 1
-                    || field[row][col+1] == 1 || field[row-1][col-1] == 1 || field[row+1][col+1] == 1
-                    || field[row+1][col-1] == 1 || field[row-1][col+1] == 1)
-                        field.at(row).at(col) = 8;
-                }
-                if (col == 0){
-                    if (field[row-1][col+1] == 1)
-                        field.at(row).at(col) = 8;
-                }
-
-                if (row == 0){
-                    if (field[row+1][col-1] == 1 || field[row][col-1] == 1)
-                        field.at(row).at(col) = 8;
-                }
-                if(row == 0 || col == 0)
-                    if (field[row][col+1] == 1 || field[row+1][col] == 1 || field[row+1][col+1] == 1)
-                        field.at(row).at(col) = 8;
-                if(row == 9 || col == 9)
-                    if (field[row][col-1] == 1 || field[row-1][col] == 1 || field[row-1][col-1] == 1)
-                        field.at(row).at(col) = 8;
+     for (int row = 1; row < field.size()-1; ++row){
+        #ifdef _WIN32
+            SetConsoleTextAttribute(hConsole, 7);
+        #endif
+        std::cout << row-1 << "  ";
+        for (int col = 1; col < field.size()-1; ++col){
+            if (field.at(row).at(col) == 1){
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 14); //set console color font green 10, yellow 14, or 22 for selected
+                #endif
+                std::cout << field.at(row).at(col) << " ";
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 7);
+                #endif
             }
-                
+            else if (field.at(row).at(col) == 8){
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 10); //set console color font green 10, yellow 14
+                #endif
+                std::cout << "." << " ";
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 7);
+                #endif
+            }
+            else{
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 8); //set console color font grey 8,
+                #endif
+                std::cout << field.at(row).at(col) << " ";
+                #ifdef _WIN32
+                SetConsoleTextAttribute(hConsole, 7);
+                #endif
+            }
         }
+        std::cout << std::endl;
     }
+    #ifdef _WIN32
+    SetConsoleTextAttribute(hConsole, 7); //set console color font white 7,
+    #endif
+    std::cout << std::endl;
 }
-
 
 void printField(std::array<std::array<int, 10>, 10> const &field){
     
@@ -135,7 +128,7 @@ void printField(std::array<std::array<int, 10>, 10> const &field){
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 14); //set console color font green 10, yellow 14, or 22 for selected
                 #endif
-                std::cout << "1" << " ";
+                std::cout << field.at(row).at(col) << " ";
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 7);
                 #endif
@@ -144,7 +137,7 @@ void printField(std::array<std::array<int, 10>, 10> const &field){
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 10); //set console color font green 10, yellow 14
                 #endif
-                std::cout << "8" << " ";
+                std::cout << "." << " ";
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 7);
                 #endif
@@ -153,7 +146,7 @@ void printField(std::array<std::array<int, 10>, 10> const &field){
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 8); //set console color font grey 8,
                 #endif
-                std::cout << "-" << " ";
+                std::cout << field.at(row).at(col) << " ";
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 7);
                 #endif
@@ -167,16 +160,73 @@ void printField(std::array<std::array<int, 10>, 10> const &field){
     std::cout << std::endl;
 }
 
-void generateShipsTemp(std::array<std::array<int, 10>, 10> &field, int ship){
 
-    checkFieldNeigh(field);
+void checkField2(std::array<std::array<int, 12>, 12> &field){
+
+    const int y[] = { -1, -1, -1,  1, 1, 1, 0, 0 };// 8 directions
+    const int x[] = { -1, 0, 1, -1, 0, 1, -1, 1 };// for checking
+
+     for(int row = 1; row < field.size()-1; ++row){
+        for(int col = 1; col < field.size()-1; ++col){
+            if (field.at(row).at(col) == 0){
+                for(int i=0; i < 8; ++i) { // looking around cell
+                    if(field.at(row+y[i]).at(col+x[i]) == 1)
+                        field.at(row).at(col) = 8;
+                    }
+            }
+
+        }
+     }
+}
+
+
+
+bool inField( int r, int c)
+{
+  if( r < 0 || r >= 9 ) return false;
+  if( c < 0 || c >= 9 ) return false;
+    return true;
+}
+void checkField(std::array<std::array<int, 10>, 10> &field){
+
+    const int y[] = { -1, -1, -1,  1, 1, 1, 0, 0 };// 8 directions
+    const int x[] = { -1, 0, 1, -1, 0, 1, -1, 1 };// for checking
+
+    //check in boundary
+
+    for(int row = 0; row < field.size(); ++row){
+        for(int col = 0; col < field.size(); ++col){
+
+            if (field.at(row).at(col) == 0){
+                for(int i=0; i < 8; ++i) { // looking around cell
+                    if (inField(row+y[i], col+x[i])){
+                        if(field.at(row+y[i]).at(col+x[i]) == 1)
+                            field.at(row).at(col) = 8;
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool checkPlace(std::array<std::array<int, 10>, 10> &field, int startPoint){
+    for (int row = 0; row < field.size(); ++row){
+        for (int col = 0; col < field.size(); ++col){
+            if(field.at(row).at(col) != startPoint)
+                return true;
+            else return false;
+        }
+    }
+}
+
+void generateShips2(std::array<std::array<int, 12>, 12> &field, int ship){
+
+    checkField2(field);
 
     int startPoint{0}, offset{0}, dir{0};
     startPoint = getRandomNumber(0, 9);
     offset = getRandomNumber(0, 9);
     dir = getRandomNumber(0, 1);
-
-    //deb(startPoint, rowOffset);
 
     if (offset + ship >= 9) offset = 5;
     
@@ -187,93 +237,105 @@ void generateShipsTemp(std::array<std::array<int, 10>, 10> &field, int ship){
             field.at(startPoint+i).at(offset) = 1;
         }
 
-
-    checkFieldNeigh(field);
 }
 
-bool inField( int r, int c)
-{
-  if( r < 0 || r >= 10 ) return false;
-  if( c < 0 || c >= 10 ) return false;
-    return true;
+void generateShips(std::array<std::array<int, 10>, 10> &field, int ship){
+
+    checkField(field);
+    int startPoint{0}, offset{0}, dir{0};
+
+    do{
+    
+        startPoint = getRandomNumber(0, 9);
+        offset = getRandomNumber(0, 9);
+        dir = getRandomNumber(0, 1);
+
+    //deb(startPoint, rowOffset);
+    } while (checkPlace(field, startPoint));
+
+    if (offset + ship >= 9) offset = 4;
+    
+    for (int i = 0; i < ship; ++i){
+        if (dir == 0) //horizontal location
+            if (field.at(startPoint).at(i+offset) != 1 && field.at(startPoint).at(i+offset) != 8)
+                field.at(startPoint).at(i+offset) = 1;
+        else //vertical
+        if (field.at(startPoint).at(i+offset) != 1 && field.at(startPoint).at(i+offset) != 8)
+            field.at(startPoint+i).at(offset) = 1;
+        }
+
+}
+
+void checkFieldNew(std::array<std::array<int, 10>, 10> &field){
+
+    for(int row = 1; row < field.size()-1; ++row){
+        for(int col = 1; col < field.size()-1; ++col){
+            int n = 0;
+			if (field[row][col] == 1) continue;
+			if (field[row+1][col] == 1) n++;
+			if (field[row][col+1] == 1) n++;
+			if (field[row-1][col] == 1) n++;
+			if (field[row][col-1] == 1) n++;
+			if (field[row+1][col+1] == 1) n++;
+			if (field[row-1][col-1] == 1) n++;
+			if (field[row-1][col+1] == 1) n++;
+			if (field[row+1][col-1] == 1) n++;
+			field[row][col] = n;
+            }
+    }
 }
 
 int main(){
 
     std::cout << std::endl << std::endl;
     srand(static_cast<unsigned int>(time(0)));
+    std::array<std::array<int, 12>, 12> field2;
     std::array<std::array<int, 10>, 10> field;
 
     createField(field);
     
-    field = {{
-              {0,0,1,0,0,0,0,0,0,0},
-              {0,0,1,0,0,0,0,0,0,0},
-              {0,0,0,0,0,0,0,0,0,0},
-              {0,0,0,0,0,0,0,0,0,0},
-              {0,0,0,0,0,0,0,0,0,0},
-              {0,1,1,1,0,0,0,0,0,0},
-              {0,0,0,0,0,0,1,0,0,0},
-              {0,0,0,0,0,0,1,0,0,0},
-              {0,0,0,0,0,0,1,0,0,0},
-              {0,0,0,0,0,0,1,0,0,0}
-            }};
+    // field2 = {{
+    //           {0,0,1,0,0,0,0,0,0,0},
+    //           {0,0,1,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,1,1,1,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0}
+    //         }};
 
-    //generateShipsTemp(field, 4);
-    //generateShipsTemp(field, 3);
+
+    // field = {{
+    //           {0,0,1,0,0,0,0,0,0,0,0,0},
+    //           {0,0,1,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0,0,0},
+    //           {0,1,1,1,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0,0,0},
+    //         }};
+
+    generateShips(field, 4);
+    generateShips(field, 3);
+    generateShips(field, 3);
+    generateShips(field, 2);
+    generateShips(field, 2);
+    generateShips(field, 2);
+    generateShips(field, 1);
+    generateShips(field, 1);
+    generateShips(field, 1);
+    generateShips(field, 1);
 
     printField(field);
-
-    const int y[] = { -1, -1, -1,  1, 1, 1, 0, 0 };// 8 directions
-    const int x[] = { -1, 0, 1, -1, 0, 1, -1, 1 };// for checking
-
-    const int y_first[] = {1, 0, 1};
-    const int x_first[] = {1, 1, 0};
-    const int x_last[]  = {-1, 0, -1};
-    const int y_last[]  = {-1, -1, 0};
-
-    const int x_first_2[] = {1, 0, -1};
-
-    //check in boundary
-
-    for(int row = 0; row < field.size(); ++row){
-        for(int col = 0; col < field.size(); ++col){
-
-            if (field.at(row).at(col) == 0){
-
-                if(row == 0 && col > 0 && col < 9){
-                    for(int f = 0; f < 3; ++f) { // looking around cell
-                        if(field.at(row+y_first[f]).at(col+x_first[f]) == 1)
-                            field.at(row).at(col) = 8;
-                    }
-                }else if (col == 0 && row > 0 && row < 9){
-                    for(int f = 0; f < 3; ++f) { // looking around cell
-                        if(field.at(row+y_first[f]).at(col+x_first[f]) == 1)
-                            field.at(row).at(col) = 8;
-                    }
-
-                }else if(row == 9 && col > 0 && col < 9){
-                    for(int l = 0; l < 3; ++l) { // looking around cell
-                        if(field.at(row+y_last[l]).at(col+x_last[l]) == 1)
-                            field.at(row).at(col) = 8;
-                    }
-
-                }else if(col == 9 && row > 0 && row < 9){
-                    for(int l = 0; l < 3; ++l) { // looking around cell
-                        if(field.at(row+y_last[l]).at(col+x_last[l]) == 1)
-                            field.at(row).at(col) = 8;
-                    }
-                }else if(row > 0 && col > 0 && row < 9 && col < 9){
-                for(int i=0; i < 8; ++i) { // looking around cell
-                    if(field.at(row+y[i]).at(col+x[i]) == 1)
-                            field.at(row).at(col) = 8;
-                            }
-                    }
-            }
-        }
-    }
-
-    //row 0 row 10 col 0 col 10
+    checkField(field);
 
     std::cout << std::endl;
 
