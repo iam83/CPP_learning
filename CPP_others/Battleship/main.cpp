38afd46d30c61a9e18afe20b7d273578a0c26177
Anton U <anton.urkin@gmail.com>
2022-09-08 23:11:58 +0300

    updat

--- CPP_others/Battleship/main.cpp
+++ CPP_others/Battleship/main.cpp
@@ -40,68 +40,8 @@ int getRandomNumber(int min, int max){
         return static_cast<int>(rand() * fraction * (max - min + 1) + min);
      }
 
-void createField(std::array<std::array<int, 12>, 12> &field){
-    field.fill({0,0});
-}
-
 void createField(std::array<std::array<int, 10>, 10> &field){
     field.fill({0,0});
-}
-
-void printField2(std::array<std::array<int, 12>, 12> const &field){
-    
-    #ifdef _WIN32
-    HANDLE  hConsole;
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    #endif
-
-    std::string letters = "ABCDEFGHKL";
-    std::cout << "   ";
-    for (auto const &lett : letters){
-        std::cout << lett << " ";
-    }
-    std::cout << std::endl;
-
-     for (int row = 1; row < field.size()-1; ++row){
-        #ifdef _WIN32
-            SetConsoleTextAttribute(hConsole, 7);
-        #endif
-        std::cout << row-1 << "  ";
-        for (int col = 1; col < field.size()-1; ++col){
-            if (field.at(row).at(col) == 1){
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 14); //set console color font green 10, yellow 14, or 22 for selected
-                #endif
-                std::cout << field.at(row).at(col) << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            else if (field.at(row).at(col) == 8){
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 10); //set console color font green 10, yellow 14
-                #endif
-                std::cout << "." << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            else{
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 8); //set console color font grey 8,
-                #endif
-                std::cout << field.at(row).at(col) << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-        }
-        std::cout << std::endl;
-    }
-    #ifdef _WIN32
-    SetConsoleTextAttribute(hConsole, 7); //set console color font white 7,
-    #endif
-    std::cout << std::endl;
 }
 
 void printField(std::array<std::array<int, 10>, 10> const &field){
@@ -160,27 +100,6 @@ void printField(std::array<std::array<int, 10>, 10> const &field){
     std::cout << std::endl;
 }
 
-
-void checkField2(std::array<std::array<int, 12>, 12> &field){
-
-    const int y[] = { -1, -1, -1,  1, 1, 1, 0, 0 };// 8 directions
-    const int x[] = { -1, 0, 1, -1, 0, 1, -1, 1 };// for checking
-
-     for(int row = 1; row < field.size()-1; ++row){
-        for(int col = 1; col < field.size()-1; ++col){
-            if (field.at(row).at(col) == 0){
-                for(int i=0; i < 8; ++i) { // looking around cell
-                    if(field.at(row+y[i]).at(col+x[i]) == 1)
-                        field.at(row).at(col) = 8;
-                    }
-            }
-
-        }
-     }
-}
-
-
-
 bool inField( int r, int c)
 {
   if( r < 0 || r >= 9 ) return false;
@@ -230,26 +149,6 @@ bool checkPlace(std::array<std::array<int, 10>, 10> &field, int startPoint, int 
     return (count > 0) ? 1 : 0;
 }
 
-void generateShips2(std::array<std::array<int, 12>, 12> &field, int ship){
-
-    checkField2(field);
-
-    int startPoint{0}, offset{0}, dir{0};
-    startPoint = getRandomNumber(0, 9);
-    offset = getRandomNumber(0, 9);
-    dir = getRandomNumber(0, 1);
-
-    if (offset + ship >= 9) offset = 5;
-    
-    for (int i = 0; i < ship; ++i){
-        if (dir == 0) //horizontal location
-            field.at(startPoint).at(i+offset) = 1;
-        else //vertical
-            field.at(startPoint+i).at(offset) = 1;
-        }
-
-}
-
 void generateShips(std::array<std::array<int, 10>, 10> &field, int ship){
 
     checkField(field);
@@ -285,7 +184,7 @@ int main(){
 
     createField(field);
     
-    // field2 = {{
+    // field = {{
     //           {0,0,1,0,0,0,0,0,0,0},
     //           {0,0,1,0,0,0,0,0,0,0},
     //           {0,0,0,0,0,0,0,0,0,0},
@@ -296,22 +195,6 @@ int main(){
     //           {0,0,0,0,0,0,1,0,0,0},
     //           {0,0,0,0,0,0,1,0,0,0},
     //           {0,0,0,0,0,0,1,0,0,0}
-    //         }};
-
-
-    // field = {{
-    //           {0,0,1,0,0,0,0,0,0,0,0,0},
-    //           {0,0,1,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0,0,0},
-    //           {0,1,1,1,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0,0,0},
     //         }};
 
     generateShips(field, 4);
