7a4a8e9fe864951f94fead049562bda542656844
AU <iam83iam@gmail.com>
2022-09-08 18:07:23 +0300

    battleship update

--- CPP_others/Battleship/main.cpp
+++ CPP_others/Battleship/main.cpp
@@ -4,10 +4,20 @@
 #include <array>
 #include <ctime>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 // A simplified version of Battleship game. An attemp to make it from scratch without looking at other examples.
 //
 // create 10x10 field
-// fill cells with values
+// fill cells with random located ships.
+//
+// 4* ships - 1
+// 3* ships - 2
+// 2* ships - 3
+// 1* ships - 4
+//
 // create game loop
 //      print your field
 //      print enemy's field
@@ -21,59 +31,148 @@
 //      else ask user to eneter coords to shot
 
 
+void deb(int deb, int deb2){
+    std::cout << "Debug: " << "< " << deb << " " << deb2 << " >" << "  ";
+}
+
 int getRandomNumber(int min, int max){
         static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
         return static_cast<int>(rand() * fraction * (max - min + 1) + min);
      }
 
-void fillIntArr(std::array<std::array<int, 10>, 10> &arr){
-    for (int row = 0; row < arr.size(); ++row){
-        for (int col = 0; col < arr.size(); ++col){
-            arr.at(row).at(col) = getRandomNumber(0, 1);
+void createField(std::array<std::array<int, 10>, 10> &field){
+    field.fill({0,0});
+}
+
+void checkFieldNeigh(std::array<std::array<int, 10>, 10> &field){
+
+    for (int row = 0; row < 10; ++row){
+        for (int col = 0; col < 10; ++col){
+
+            if (field.at(row).at(col) == 0){
+                if (row > 0 && row < 10 && col > 0 && col < 10){
+                    if (field[row-1][col] == 1 || field[row+1][col] == 1 || field[row][col-1] == 1
+                    || field[row][col+1] == 1 || field[row-1][col-1] == 1 || field[row+1][col+1] == 1
+                    || field[row+1][col-1] == 1 || field[row-1][col+1] == 1)
+                        field.at(row).at(col) = 8;
+                }
+                if (col == 0){
+                    if (field[row-1][col+1] == 1)
+                        field.at(row).at(col) = 8;
+                }
+
+                if (row == 0){
+                    if (field[row+1][col-1] == 1 || field[row][col-1] == 1)
+                        field.at(row).at(col) = 8;
+                }
+                if(row == 0 || col == 0)
+                    if (field[row][col+1] == 1 || field[row+1][col] == 1 || field[row+1][col+1] == 1)
+                        field.at(row).at(col) = 8;
+                if(row == 10 || col == 10)
+                    if (field[row][col-1] == 1 || field[row-1][col] == 1 || field[row-1][col-1] == 1)
+                        field.at(row).at(col) = 8;
+            }
+                
         }
     }
 }
 
-void fillStrArr(std::array<std::array<char, 10>, 10> &arr_str, char sign){
-    for (int row = 0; row < arr_str.size(); ++row){
-        for (int col = 0; col < arr_str.size(); ++col){
-            arr_str.at(row).at(col) = sign;
-        }
-    }
-}
+void printField(std::array<std::array<int, 10>, 10> const &field){
+    
+    #ifdef _WIN32
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    #endif
 
-void printStrArr(std::array<std::array<char, 10>, 10> const &arr_str){
-     for (int row = 0; row < arr_str.size(); ++row){
-        for (int col = 0; col < arr_str.size(); ++col){
-            std::cout << arr_str.at(row).at(col);
+    std::string letters = "ABCDEFGHKL";
+    std::cout << "   ";
+    for (auto const &lett : letters){
+        std::cout << lett << " ";
+    }
+    std::cout << std::endl;
+
+     for (int row = 0; row < field.size(); ++row){
+        SetConsoleTextAttribute(hConsole, 7);
+        std::cout << row << "  ";
+        for (int col = 0; col < field.size(); ++col){
+            if (field.at(row).at(col) == 1){
+                SetConsoleTextAttribute(hConsole, 14); //set console color font green 10, yellow 14, or 22 for selected
+                std::cout << field.at(row).at(col) << " ";
+                SetConsoleTextAttribute(hConsole, 7);
+            }
+            else if (field.at(row).at(col) == 8){
+                SetConsoleTextAttribute(hConsole, 10); //set console color font green 10, yellow 14
+                std::cout << field.at(row).at(col) << " ";
+                SetConsoleTextAttribute(hConsole, 7);
+            }
+            else{
+                SetConsoleTextAttribute(hConsole, 8); //set console color font grey 8,
+                std::cout << field.at(row).at(col) << " ";
+                SetConsoleTextAttribute(hConsole, 7);
+            }
         }
         std::cout << std::endl;
     }
+    
+    SetConsoleTextAttribute(hConsole, 7); //set console color font white 7,
+    std::cout << std::endl;
 }
 
-void printIntArr(std::array<std::array<int, 10>, 10> const &arr){
-     for (int row = 0; row < arr.size(); ++row){
-        for (int col = 0; col < arr.size(); ++col){
-            std::cout << arr.at(row).at(col);
+void generateShipsTemp(std::array<std::array<int, 10>, 10> &field, int ship){
+
+    checkFieldNeigh(field);
+
+    int startPoint{0}, offset{0}, dir{0};
+
+    startPoint = getRandomNumber(0, 9);
+    startPoint = getRandomNumber(0, 9);
+    offset = getRandomNumber(0, 9);
+    dir = getRandomNumber(0, 1);
+
+    //deb(startPoint, rowOffset);
+
+
+    if (offset + ship >= 10) offset = 6;{
+        for (int i = 0; i < ship; ++i){
+            if (dir == 0)
+                field.at(startPoint).at(i+offset) = 1;
+            else
+                field.at(i+offset).at(startPoint) = 1;
+            }
         }
-        std::cout << std::endl;
-    }
+
+    checkFieldNeigh(field);
 }
+
 
 int main(){
 
+    std::cout << std::endl << std::endl;
     srand(static_cast<unsigned int>(time(0)));
+    std::array<std::array<int, 10>, 10> field;
 
-    std::array<std::array<int, 10>, 10> arr;
-    std::array<std::array<char, 10>, 10> arr_str;
-
-    //fillIntArr(arr);
-    //printIntArr(arr);
-
-    char sign = '-';
-    fillStrArr(arr_str, sign);
-    arr_str.at(1).at(2) = '*';
-    printStrArr(arr_str);
+    createField(field);
     
+    // field = {{
+    //           {0,0,1,0,0,0,0,0,0,0},
+    //           {0,0,1,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,0,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0},
+    //           {0,0,0,0,0,0,1,0,0,0}
+    //         }};
+
+    printField(field);
+    generateShipsTemp(field, 4);
+    generateShipsTemp(field, 3);
+    printField(field);
+
+    if (field.at(2).at(5) == 1)
+        std::cout << "Gotcha\n";
+
     return 0;
 }
