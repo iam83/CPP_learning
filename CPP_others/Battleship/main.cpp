d7ba2026388817f527d94d793df2b92dddf2ca18
AU <iam83iam@gmail.com>
2022-09-08 19:38:35 +0300

    battleshipt update

--- CPP_others/Battleship/main.cpp
+++ CPP_others/Battleship/main.cpp
@@ -44,13 +44,46 @@ void createField(std::array<std::array<int, 10>, 10> &field){
     field.fill({0,0});
 }
 
-void checkFieldNeigh(std::array<std::array<int, 10>, 10> &field){
+void checkFieldNeigh2(std::array<std::array<int, 10>, 10> &field){
 
-    for (int row = 0; row < 10; ++row){
-        for (int col = 0; col < 10; ++col){
+    for (int row = 0; row < 9; ++row){
+        for (int col = 0; col < 9; ++col){
 
             if (field.at(row).at(col) == 0){
-                if (row > 0 && row < 10 && col > 0 && col < 10){
+                if (row >=1 && row <= 8 && col >= 1 && col <= 8){
+                    if (field.at(row-1).at(col) == 1 || field.at(row+1).at(col) == 1 || field.at(row).at(col-1) == 1
+                    || field.at(row).at(col+1) == 1 || field.at(row-1).at(col-1) == 1 || field.at(row+1).at(col+1) == 1
+                    || field.at(row+1).at(col-1) == 1 || field.at(row-1).at(col+1) == 1)
+                        field.at(row).at(col) = 8;
+                }
+                if (col == 0){
+                    if (field.at(row-1).at(col+1) == 1)
+                        field.at(row).at(col) = 8;
+                }
+
+                if (row == 0){
+                    if (field.at(row+1).at(col-1) == 1 || field.at(row).at(col-1) == 1)
+                        field.at(row).at(col) = 8;
+                }
+                if(row == 0 || col == 0)
+                    if (field.at(row).at(col+1) == 1 || field.at(row+1).at(col) == 1 || field.at(row+1).at(col+1) == 1)
+                        field.at(row).at(col) = 8;
+                if(row == 9 || col == 9)
+                    if (field.at(row).at(col-1) == 1 || field.at(row-1).at(col) == 1 || field.at(row-1).at(col-1) == 1)
+                        field.at(row).at(col) = 8;
+            }
+                
+        }
+    }
+}
+
+void checkFieldNeigh(std::array<std::array<int, 10>, 10> &field){
+
+    for (int row = 0; row < field.size(); ++row){
+        for (int col = 0; col < field.size(); ++col){
+
+            if (field.at(row).at(col) == 0){
+                if (row > 0 && row <= 8 && col > 0 && col <= 8){
                     if (field[row-1][col] == 1 || field[row+1][col] == 1 || field[row][col-1] == 1
                     || field[row][col+1] == 1 || field[row-1][col-1] == 1 || field[row+1][col+1] == 1
                     || field[row+1][col-1] == 1 || field[row-1][col+1] == 1)
@@ -68,7 +101,7 @@ void checkFieldNeigh(std::array<std::array<int, 10>, 10> &field){
                 if(row == 0 || col == 0)
                     if (field[row][col+1] == 1 || field[row+1][col] == 1 || field[row+1][col+1] == 1)
                         field.at(row).at(col) = 8;
-                if(row == 10 || col == 10)
+                if(row == 9 || col == 9)
                     if (field[row][col-1] == 1 || field[row-1][col] == 1 || field[row-1][col-1] == 1)
                         field.at(row).at(col) = 8;
             }
@@ -76,6 +109,7 @@ void checkFieldNeigh(std::array<std::array<int, 10>, 10> &field){
         }
     }
 }
+
 
 void printField(std::array<std::array<int, 10>, 10> const &field){
     
@@ -123,27 +157,31 @@ void generateShipsTemp(std::array<std::array<int, 10>, 10> &field, int ship){
     checkFieldNeigh(field);
 
     int startPoint{0}, offset{0}, dir{0};
-
-    startPoint = getRandomNumber(0, 9);
     startPoint = getRandomNumber(0, 9);
     offset = getRandomNumber(0, 9);
     dir = getRandomNumber(0, 1);
 
     //deb(startPoint, rowOffset);
 
-
-    if (offset + ship >= 10) offset = 6;{
-        for (int i = 0; i < ship; ++i){
-            if (dir == 0)
-                field.at(startPoint).at(i+offset) = 1;
-            else
-                field.at(i+offset).at(startPoint) = 1;
-            }
+    if (offset + ship >= 9) offset = 5;
+    
+    for (int i = 0; i < ship; ++i){
+        if (dir == 0) //horizontal location
+            field.at(startPoint).at(i+offset) = 1;
+        else //vertical
+            field.at(startPoint+i).at(offset) = 1;
         }
+
 
     checkFieldNeigh(field);
 }
 
+bool inField( int r, int c)
+{
+  if( r < 0 || r >= 10 ) return false;
+  if( c < 0 || c >= 10 ) return false;
+    return true;
+}
 
 int main(){
 
@@ -153,26 +191,41 @@ int main(){
 
     createField(field);
     
-    // field = {{
-    //           {0,0,1,0,0,0,0,0,0,0},
-    //           {0,0,1,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,0,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0},
-    //           {0,0,0,0,0,0,1,0,0,0}
-    //         }};
+    field = {{
+              {0,0,1,0,0,0,0,0,0,0},
+              {0,0,1,0,0,0,0,0,0,0},
+              {0,0,0,0,0,0,0,0,0,0},
+              {0,0,0,0,0,0,0,0,0,0},
+              {0,0,0,0,0,0,0,0,0,0},
+              {0,1,1,1,0,0,0,0,0,0},
+              {0,0,0,0,0,0,1,0,0,0},
+              {0,0,0,0,0,0,1,0,0,0},
+              {0,0,0,0,0,0,1,0,0,0},
+              {0,0,0,0,0,0,1,0,0,0}
+            }};
+
+    //generateShipsTemp(field, 4);
+    //generateShipsTemp(field, 3);
 
     printField(field);
-    generateShipsTemp(field, 4);
-    generateShipsTemp(field, 3);
-    printField(field);
 
-    if (field.at(2).at(5) == 1)
-        std::cout << "Gotcha\n";
+    const int y[] = { -1, -1, -1,  1, 1, 1,  0, 0 };// 8 shifts to neighbors
+    const int x[] = { -1,  0,  1, -1, 0, 1, -1, 1 };// used in functions 
+
+    for(int row = 1; row < field.size()-1; ++row){
+        for(int col = 1; col < field.size()-1; ++col){
+            for(int i=0; i<8; ++i) { // visit the 8 spaces around it
+                if (field.at(row).at(col) == 0){
+                    if(field.at(row+y[i]).at(col+x[i]) == 1)
+                        field.at(row).at(col) = 8;
+                }
+            }
+        }
+    }
+
+    std::cout << std::endl;
+
+    printField(field);
 
     return 0;
 }
