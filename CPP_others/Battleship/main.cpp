30772adddb391dce00ee6ab8b965d3a682e4144c
Anton <iam83iam@gmail.com>
2022-09-11 23:19:41 +0300

    update

--- CPP_others/Battleship/main.cpp
+++ CPP_others/Battleship/main.cpp
@@ -102,6 +102,7 @@ bool inField( int r, int c)
   if( c < 0 || c >= 9 ) return false;
     return true;
 }
+
 void checkField(std::array<std::array<int, 10>, 10> &field){
 
     const int y[] = { -1, -1, -1, 1, 1, 1, 0, 0 };// 8 directions
@@ -124,6 +125,57 @@ void checkField(std::array<std::array<int, 10>, 10> &field){
     }
 }
 
+bool checkCells(std::array<std::array<int, 10>, 10> &field, int row, int col, int ship, int dir){
+
+    const int y[] = { -1, -1, -1, 1, 1, 1, 0, 0 };// 8 directions
+    const int x[] = { -1, 0, 1, -1, 0, 1, -1, 1 };// for checking
+
+    for (int s = 0; s < ship; ++s){
+        if (dir == 0){
+            for(int i=0; i < 8; ++i) {
+                if (inField(row+y[i], col+x[i]+s)){
+                    if(field.at(row+y[i]).at(col+x[i]+s) == 1 || field.at(row+y[i]).at(col+x[i]+s) == 8)
+                        std::cout << "checkCells: FALSE\n";
+                        return false;
+                    }
+                    else{
+                        std::cout << "checkCells: TRUE\n";
+                        return true;
+                    }
+            }
+        }else{
+            for(int i=0; i < 8; ++i) {
+                if (inField(row+y[i]+s, col+x[i])){
+                    if(field.at(row+y[i]+s).at(col+x[i]) == 1 || field.at(row+y[i]+s).at(col+x[i]) == 8)
+                        std::cout << "checkCells: FALSE\n";
+                        return false;
+                    }
+                    else{
+                        std::cout << "checkCells: TRUE\n";
+                        return true;
+                    }
+            }
+        }
+    }
+}
+
+bool checkCells2(std::array<std::array<int, 10>, 10> &field, int row, int col, int ship, int dir){
+
+    const int y[] = { -1, -1, -1, 1, 1, 1, 0, 0 };// 8 directions
+    const int x[] = { -1, 0, 1, -1, 0, 1, -1, 1 };// for checking
+    for(int i=0; i < 8; ++i) {
+     if (inField(row+y[i], col+x[i])){
+        if(field.at(row+y[i]).at(col+x[i]) == 1 || field.at(row+y[i]).at(col+x[i]) == 8)
+            std::cout << "checkCells: FALSE\n";
+                return false;
+            }else{
+                std::cout << "checkCells: TRUE\n";
+                return true;
+                }
+    }
+}
+
+
 bool checkPlace(std::array<std::array<int, 10>, 10> &field, int startPoint, int offset, int ship, int dir){
 
     int count{0};
@@ -140,11 +192,12 @@ bool checkPlace(std::array<std::array<int, 10>, 10> &field, int startPoint, int 
     std::cout << "offset = " << offset << std::endl;
     std::cout << "direction = " << temp_dir << std::endl;
 
-    if (field.at(startPoint).at(offset) != 1 && field.at(startPoint).at(offset) != 8){
-        std::cout << "true \n\n";
-        return true;
+
+    if (field.at(startPoint).at(offset) != 1 && field.at(startPoint).at(offset) != 8 && (startPoint+ship) < 9 && (offset+ship) < 9){
+            std::cout << "TRUE \n\n";
+            return true;
     }else{
-        std::cout << "false \n\n";
+        std::cout << "FALSE \n\n";
         return false;
     }
 /*
@@ -224,6 +277,13 @@ int main(){
     generateShips(field, 4);
     generateShips(field, 3);
     generateShips(field, 3);
+    generateShips(field, 2);
+    generateShips(field, 2);
+    generateShips(field, 2);
+    generateShips(field, 1);
+    generateShips(field, 1);
+    generateShips(field, 1);
+    generateShips(field, 1);
 
     std::cout << std::endl;
 
