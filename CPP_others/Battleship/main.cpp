8a81b1df06dbb5f10eac123c7a5bf51be90ea347
Anton <iam83iam@gmail.com>
2022-09-17 19:09:24 +0300

    update

--- CPP_others/Battleship/main.cpp
+++ CPP_others/Battleship/main.cpp
@@ -80,23 +80,23 @@ void printTwoFields(std::array<std::array<int, 10>, 10> const &field_pc, std::ar
     std::string separator = "        ";
     std::cout << "      ";
 
-    for (auto const &lett : letters){
-        std::cout << lett << " ";
+    for (int c = 0; c < 10; ++c){
+        std::cout << c << " ";
     }
 
     std::cout << separator + "   ";
 
-    for (auto const &lett : letters){
-        std::cout << lett << " ";
+    for (int c = 0; c < 10; ++c){
+        std::cout << c << " ";
     }
 
     std::cout << std::endl;
 
-    for (int row = 0; row < field_pc.size(); ++row){
-        std::cout << "   " << row << "  "; //row number
+    for (int row = 0; row < field_user.size(); ++row){
+        std::cout << "   " << letters[row] << "  "; //row number
         
-        for (int col = 0; col < field_pc.size(); ++col){// user field
-            //std::cout << field_user.at(row).at(col) << " ";
+        // user field
+        for (int col = 0; col < field_user.size(); ++col){
              if (field_user.at(row).at(col) == static_cast<int>(FieldCellStates::Ship)){
                 #ifdef _WIN32
                     SetConsoleTextAttribute(hConsole, 14); //set console color font green 10, yellow 14, or 22 for selected
@@ -160,11 +160,10 @@ void printTwoFields(std::array<std::array<int, 10>, 10> const &field_pc, std::ar
             }
 
         std::cout << separator;
-        std::cout << row << "  ";
+        std::cout << letters[row] << "  ";
 
-        for (int col = 0; col < field_pc.size(); ++col){ // pc field
-            //std::cout << field_pc.at(row).at(col) << " ";
-            
+        // pc field
+        for (int col = 0; col < field_pc.size(); ++col){            
             if (field_pc.at(row).at(col) == static_cast<int>(FieldCellStates::Ship)){
                 #ifdef _WIN32
                 SetConsoleTextAttribute(hConsole, 8); //set console color font green 10, yellow 14, 11 light blue, 13 magenta, 9 dark blue or 22 for selected                    
@@ -228,103 +227,6 @@ void printTwoFields(std::array<std::array<int, 10>, 10> const &field_pc, std::ar
         std::cout << std::endl;
     }
     
-    #ifdef _WIN32
-    SetConsoleTextAttribute(hConsole, 7); //set console color font white 7,
-    #endif
-    std::cout << std::endl;
-}
-
-//print one field
-void printField(std::array<std::array<int, 10>, 10> const &field, Player player){
-    
-    #ifdef _WIN32
-    HANDLE  hConsole;
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    #endif
-
-    std::string letters = "ABCDEFGHIJ";
-    //std::string letters = "0123456789"; //for debugging
-    std::cout << "   ";
-    for (auto const &lett : letters){
-        std::cout << lett << " ";
-    }
-    std::cout << std::endl;
-
-     for (int row = 0; row < field.size(); ++row){
-        #ifdef _WIN32
-            SetConsoleTextAttribute(hConsole, 7);
-        #endif
-        std::cout << row << "  ";
-        for (int col = 0; col < field.size(); ++col){
-            //if cells are taken by ships
-            if (field.at(row).at(col) == static_cast<int>(FieldCellStates::Ship)){
-                #ifdef _WIN32
-                if(player == Player::User)
-                    SetConsoleTextAttribute(hConsole, 14); //set console color font green 10, yellow 14, or 22 for selected
-                else
-                    SetConsoleTextAttribute(hConsole, 8); //set console color font green 10, yellow 14, 11 light blue, 13 magenta, 9 dark blue or 22 for selected                    
-                #endif
-                if (player == Player::User)
-                    std::cout << "#" << " ";
-                else
-                    std::cout << "." << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            //border around ship
-            else if (field.at(row).at(col) == static_cast<int>(FieldCellStates::Border)){
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 8); //set console color font green 10, yellow 14
-                #endif
-                std::cout << "." << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            //ship is hit
-            else if (field.at(row).at(col) == static_cast<int>(FieldCellStates::Hit)){
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 12); //red
-                #endif
-                std::cout << "X" << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            //border around hitted ship
-            else if (field.at(row).at(col) == static_cast<int>(FieldCellStates::BorderHit)){
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 8); 
-                #endif
-                std::cout << "-" << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            //missed hit
-            else if (field.at(row).at(col) == static_cast<int>(FieldCellStates::Miss)){
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 11); //light blue
-                #endif
-                std::cout << "-" << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-            //just empty field
-            else{
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 8); //set console color font grey 8,
-                #endif
-                std::cout << "." << " ";
-                #ifdef _WIN32
-                SetConsoleTextAttribute(hConsole, 7);
-                #endif
-            }
-        }
-        std::cout << std::endl;
-    }
     #ifdef _WIN32
     SetConsoleTextAttribute(hConsole, 7); //set console color font white 7,
     #endif
@@ -528,15 +430,151 @@ bool isInputValid(std::string &coord){ //check if user makes correct input
     return false;
 }
 
-bool pcMove(std::array<std::array<int, 10>, 10> const &field_user){
+bool pcMove(std::array<std::array<int, 10>, 10> &field_user, std::string &pcLastMove){
+
+    std::cout << std::endl;
+    std::cout << "   PC is hitting";
+
+    for (int c = 0; c < 3; ++c){
+        std::cout << ".";
+        Sleep(800);
+    }
 
     int row = getRandomNumber(0, 9);
     int col = getRandomNumber(0, 9);
-    if (field_user.at(row).at(col) == 1){
-        return true;
-    }
-    return false;
+
+    std::string letters = "ABCDEFGHIJ";
+
+    std::cout << letters[row] << col;
+
+    pcLastMove = letters[row] + std::to_string(col);
+
+    Sleep(1250);
+
+    if (field_user.at(row).at(col) == static_cast<int>(FieldCellStates::Ship)){
+            field_user.at(row).at(col) = static_cast<int>(FieldCellStates::Hit);
+            return true;
+    }else{
+            if(field_user.at(row).at(col) != static_cast<int>(FieldCellStates::Hit) &&
+               field_user.at(row).at(col) != static_cast<int>(FieldCellStates::Miss)) {
+                field_user.at(row).at(col) = static_cast<int>(FieldCellStates::Miss);
+            }
+            
+        }
+
+        return false;
 }
+
+void decodeCoords(std::string coord, int &row, int &col){
+    switch(coord[0]){
+            case 'A':
+                row = 0;
+                break;
+            case 'B':
+                row = 1;
+                break;
+            case 'C':
+                row = 2;
+                break;
+            case 'D':
+                row = 3;
+                break;
+            case 'E':
+                row = 4;
+                break;
+            case 'F':
+                row = 5;
+                break;
+            case 'G':
+                row = 6;
+                break;
+            case 'H':
+                row = 7;
+                break;
+            case 'I':
+                row = 8;
+                break;
+            case 'J':
+                row = 9;
+                break;
+        }
+
+        switch(coord[1]){
+            case '0':
+                col = 0;
+                break;
+            case '1':
+                col = 1;
+                break;
+            case '2':
+                col = 2;
+                break;
+            case '3':
+                col = 3;
+                break;
+            case '4':
+                col = 4;
+                break;
+            case '5':
+                col = 5;
+                break;
+            case '6':
+                col = 6;
+                break;
+            case '7':
+                col = 7;
+                break;
+            case '8':
+                col = 8;
+                break;
+            case '9':
+                col = 9;
+                break;
+        }
+}
+
+void test(){
+
+    std::vector<std::pair<int, int>> pc_moves;
+
+    for (int i = 0; i <= 9; ++i){
+        for(int j = 0; j <= 9; ++j){
+            pc_moves.push_back(std::make_pair(i, j));
+        }
+    }
+
+    int c = 0;
+    for (auto const &v : pc_moves){
+            std::cout << v.first << "." << v.second << " ";
+            ++c;
+            if (c % 10 == 0) std::cout << std::endl;
+        }
+
+    std::cout << std::endl;
+
+    std::cout << "pc_moves: " << pc_moves.at(5).second << std::endl;
+
+    std::cout << std::endl;
+
+    int t_row = rand() % 9;
+    int t_col = rand() % 9;
+    std::cout << "t_row " << t_row << "  t_col " << t_col << std::endl;
+
+    std::cout << pc_moves[t_row].second << ".";
+    std::cout << pc_moves[t_col].second << std::endl;
+
+    pc_moves.erase(pc_moves.begin() + t_row);
+
+
+
+    c = 0;
+    for (auto const &v : pc_moves){
+            std::cout << v.first << "." << v.second << " ";
+            ++c;
+            if (c % 10 == 0) std::cout << std::endl;
+        }
+}
+
 
 int main(){
 
@@ -546,6 +584,9 @@ int main(){
     std::array<std::array<int, 10>, 10> field_user;
     std::array<std::array<int, 10>, 10> field_pc;
     std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> vec;
+    
+    test();
+
     int dir{0};
     
     // field_pc = {{
@@ -564,8 +605,8 @@ int main(){
     createGameField(field_pc, vec, dir);
     createGameField(field_user, vec, dir);
     checkField(field_pc);
-    //printField(field_pc, Player::Pc);
-    //printField(field_user, Player::User);
+    checkField(field_user);
+
     printTwoFields(field_pc, field_user);
     
     int row, col;
@@ -587,75 +628,13 @@ int main(){
 
         system(CLS);
 
-        std::string lastMove = coord;
+        std::string userLastMove = coord;
+        std::string pcLastMove;
 
-        switch(coord[0]){
-            case 'A':
-                col = 0;
-                break;
-            case 'B':
-                col = 1;
-                break;
-            case 'C':
-                col = 2;
-                break;
-            case 'D':
-                col = 3;
-                break;
-            case 'E':
-                col = 4;
-                break;
-            case 'F':
-                col = 5;
-                break;
-            case 'G':
-                col = 6;
-                break;
-            case 'H':
-                col = 7;
-                break;
-            case 'I':
-                col = 8;
-                break;
-            case 'J':
-                col = 9;
-                break;
-        }
-
-        switch(coord[1]){
-            case '0':
-                row = 0;
-                break;
-            case '1':
-                row = 1;
-                break;
-            case '2':
-                row = 2;
-                break;
-            case '3':
-                row = 3;
-                break;
-            case '4':
-                row = 4;
-                break;
-            case '5':
-                row = 5;
-                break;
-            case '6':
-                row = 6;
-                break;
-            case '7':
-                row = 7;
-                break;
-            case '8':
-                row = 8;
-                break;
-            case '9':
-                row = 9;
-                break;
-        }
+        decodeCoords(coord, row, col);
 
         checkField(field_pc);
+        checkField(field_user);
 
         std::string message;
 
@@ -672,15 +651,25 @@ int main(){
             }
         }
 
-        //printField(field_pc, Player::Pc);
-        //printField(field_user, Player::User);
-
         printTwoFields(field_pc, field_user);
 
         std::cout << message << std::endl;
-        std::cout << "   Your last move: " << lastMove << std::endl << std::endl;
+        std::cout << "   Your last move: " << userLastMove << std::endl;
 
-        //pc move
+        //pc move to be REFACTORED
+        if (pcMove(field_user, pcLastMove)){
+            message = "   !!! You got hit !!!";
+            std::cout << '\a';
+        }else{
+            message = "    PC missed! ";
+        }
+        
+        system(CLS);
+        printTwoFields(field_pc, field_user);
+
+        std::cout << message << std::endl;
+        std::cout << "   Your last move: " << userLastMove << std::endl;
+        std::cout << "     PC last move: " << pcLastMove << std::endl << std::endl;
 
     }
 
