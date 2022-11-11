8aec02f8a3101e6c0fdefd0fa8400e5be5985dc5
iam83 <iam83iam@gmail.com>
2022-11-11 17:58:56 +0300

    update

--- CPP_temp/main.cpp
+++ CPP_temp/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main(){
-    
+
     int number = 10;
     number++;
+    ++number;
     std::cout << number << std::endl;
     std::cout << "hello" << std::endl;
     std::cout << "world" << std::endl;
