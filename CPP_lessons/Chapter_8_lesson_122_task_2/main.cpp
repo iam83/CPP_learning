4ecf34c257e13a7c36ae03a102b4c89bd340bb0e
iam83 <iam83iam@gmail.com>
2023-06-05 17:09:52 +0300

    update

--- CPP_lessons/Chapter_8_lesson_122_task_2/main.cpp
+++ CPP_lessons/Chapter_8_lesson_122_task_2/main.cpp
@@ -5,31 +5,56 @@
     значения для m_a, m_b и m_c;
     - открытый метод с именем print(), который будет выводить объект класса 
     Numbers в следующем формате: <m_a, m_b, m_c>
+
+    b) Добавьте функцию isEqual() в класс Numbers, чтобы следующий код работал 
+    корректно:
 */
 
 #include <iostream>
 
 class Numbers{
 
-    int m_a, m_b, m_c;
+    double m_a, m_b, m_c;
 
 public:
 
-    void setValues(int a, int b, int c){
+    void setValues(double a, double b, double c){
         m_a = a;
         m_b = b;
         m_c = c;
     }
 
+    bool isEqual(Numbers &b){
+        return (m_a == b.m_a && m_b == b.m_b && m_c == b.m_c);
+    }
+
+
     void print(){
-        std::cout << "<" << m_a << ", " << m_b << ", " << m_c << std::endl;
+        std::cout << "<" << m_a << ", " << m_b << ", " << m_c << ">" << std::endl;
     }
 
 };
 
-
 int main(){
 
+    Numbers point1; 
+    point1.setValues(3.0, 4.0, 5.0); 
+    
+    Numbers point2; 
+    point2.setValues(3.0, 4.0, 5.0); 
+    
+    if (point1.isEqual(point2)) 
+        std::cout << "point1 and point2 are equal\n"; 
+    else 
+        std::cout << "point1 and point2 are not equal\n"; 
+    
+    Numbers point3; 
+    point3.setValues(7.0, 8.0, 9.0); 
 
+    if (point1.isEqual(point3)) 
+        std::cout << "point1 and point3 are equal\n"; 
+    else 
+        std::cout << "point1 and point3 are not equal\n";
     return 0;
+
 }
