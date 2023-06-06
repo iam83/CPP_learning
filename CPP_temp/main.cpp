4edd41abfb4de477b77518d7f61a148e8592cd70
Anton U <anton.urkin@gmail.com>
2023-06-06 19:13:16 +0300

    update

--- CPP_temp/main.cpp
+++ CPP_temp/main.cpp
@@ -4,6 +4,7 @@ int main(){
 
     int number = 10;
     number++;
+    std::cout << number << std::endl;
     ++number;
     std::cout << number << std::endl;
     std::cout << "hello" << std::endl;
