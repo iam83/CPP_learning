004096fb6dbd944ea90772fe184f0a01539c4b68
iam83 <iam83iam@gmail.com>
2023-06-09 13:19:06 +0300

    update main.cpp

--- CPP_others/Class_Geometry/main.cpp
+++ CPP_others/Class_Geometry/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 
 
+class Box{
+
+public:
+    double length;
+    double width;
+    double height;
+    
+    //create default constructor
+    Box(double l = 10, double w = 10, double h = 10)
+        : length(l), width(w), height(h)
+    {    
+    }
+    
+
+    void print() {
+        std::cout << "Length: " << length << '\n'
+                << "Width: " << width << '\n'
+                << "Height: " << height << '\n' << '\n';
+    }
+
 int main(){
 
+    Box box1;
 
+    Box box2(20);
+
+    box1.print();
+    box2.print();
+
+    return 0;
 
 }
