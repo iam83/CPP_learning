cc13d1901a6559176ef2b4f5761fbb556cda8b7c
Anton U <anton.urkin@gmail.com>
2023-07-23 18:33:07 +0300

    update main and main.cpp

--- CPP_others/FinCalc/main.cpp
+++ CPP_others/FinCalc/main.cpp
@@ -14,9 +14,10 @@ void clearScreen(){
     #endif 
 
 }
+
 void printResult(double overall_income, double month_income, int count){
-    std::cout << std::showbase << "Month " << count+1 << ": " << std::put_money(overall_income)
-            << " per month: " << std::put_money(static_cast<long double>(month_income)) << "\n";
+    std::cout << std::showbase << "Month " << std::right << std::setw(2) << count+1 << ": " << std::put_money(overall_income * 100)
+            << " per month: " << std::put_money(month_income * 100) << "\n";
 }
 
 int main(){
@@ -26,24 +27,24 @@ int main(){
     double overall_income{0.0};
     double const interest = 14.5;
     double month_income{0.0};
-    //double sum_start = EnterValues("Enter your deposit:> ");
-    //double month_amount = EnterValues("How many month(s)?> ");
+    // double start_amount = EnterValues("Enter your deposit:> ");
+    // double month_amount = EnterValues("How many month(s)?> ");
 
-    double sum_start = 50000.75;
+    double start_amount = 5000000.0;
     double month_amount = 12.0;
 
-    //std::cout << std::fixed << std::setprecision(2);
+    std::cout << std::fixed << std::setprecision(2);
     std::cout.imbue(std::locale("ru_RU.UTF-8"));
-    std::cout << std::showbase << "Start amount: " << std::put_money(sum_start) << "\n";
+    std::cout << std::showbase << "Start amount: " << std::put_money(start_amount * 100) << "\n";
     std::cout << "Interest rate: " << interest << "%\n\n";
 
     for (int i = 0; i < month_amount; ++i){
 
         //for the first month
         if (i == 0){
-            month_income = (sum_start * (interest / 12.0)) / 100.0;
-            overall_income = month_income + sum_start;
-            printResult(sum_start, month_income, i);
+            month_income = (start_amount * (interest / 12.0)) / 100.0;
+            overall_income = month_income + start_amount;
+            printResult(overall_income, month_income, i);
             continue;
         }
 
@@ -67,7 +68,7 @@ int main(){
               << " month(s) is: " << overall_income;
 
     std::cout << "\nYour income for " << month_amount
-              << " month(s) is: " << "\033[32m" << overall_income - sum_start << "\033[0m\n";
+              << " month(s) is: " << "\033[32m" << overall_income - start_amount << "\033[0m\n";
     std::cout << "\n\n";
     #endif
 
---
+++ CPP_others/FinCalc/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+    double start_amount = 5000000.0;
+    double month_amount = 12.0;
