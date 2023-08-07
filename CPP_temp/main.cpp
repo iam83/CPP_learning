fefd382f9d5005712c625a794eb877af6c28e1b4
iam83 <iam83iam@gmail.com>
2023-08-07 14:56:32 +0300

    update main.cpp

--- CPP_temp/main.cpp
+++ CPP_temp/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include <numbers>
+#include <numbers> 
 #include <format>
 
 int main(){
-
+    int temp {5};
     std::cout << std::numbers::phi << "\n";
-    std::cout << std::format("Hello {}", 10);
+    std::cout << std::format("Hello {}", temp);
     return 0;
 }
