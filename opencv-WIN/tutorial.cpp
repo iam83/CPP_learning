efe260cff9090cb23e48b308187527b9a1dfe52c
iam83 <iam83iam@gmail.com>
2024-03-15 14:31:53 +0300

    update .ninja_log, build.ninja and tutorial.cpp

--- opencv-WIN/tutorial.cpp
+++ opencv-WIN/tutorial.cpp
@@ -1,163 +1,57 @@
-#include <opencv2/objdetect.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <iostream>
-#include <iomanip>
+#include "opencv2\opencv.hpp"
+#include "opencv2\highgui.hpp"
+#include "opencv2\tracking.hpp"
 
-using namespace cv;
-using namespace std;
+int main() {
 
-class Detector
-{
-    enum Mode { Default, Daimler } m;
-    HOGDescriptor hog, hog_d;
-public:
-    Detector() : m(Default), hog(), hog_d(Size(48, 96), Size(16, 16), Size(8, 8), Size(8, 8), 9)
-    {
-        hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
-        hog_d.setSVMDetector(HOGDescriptor::getDaimlerPeopleDetector());
-    }
-    void toggleMode() { m = (m == Default ? Daimler : Default); }
-    string modeName() const { return (m == Default ? "Default" : "Daimler"); }
-    vector<Rect> detect(InputArray img)
-    {
-        // Run the detector with default parameters. to get a higher hit-rate
-        // (and more false alarms, respectively), decrease the hitThreshold and
-        // groupThreshold (set groupThreshold to 0 to turn off the grouping completely).
-        vector<Rect> found;
-        if (m == Default)
-            hog.detectMultiScale(img, found, 0, Size(8,8), Size(), 1.05, 2, false);
-        else if (m == Daimler)
-            hog_d.detectMultiScale(img, found, 0, Size(8,8), Size(), 1.05, 2, true);
-        return found;
-    }
-    void adjustRect(Rect & r) const
-    {
-        // The HOG detector returns slightly larger rectangles than the real objects,
-        // so we slightly shrink the rectangles to get a nicer output.
-        r.x += cvRound(r.width*0.1);
-        r.width = cvRound(r.width*0.8);
-        r.y += cvRound(r.height*0.07);
-        r.height = cvRound(r.height*0.8);
-    }
-};
-static const string keys = "{ help h   |   | print help message }"
-                           "{ camera c | 0 | capture video from camera (device index starting from 0) }"
-                           "{ video v  |   | use video as input }";
-int main(int argc, char** argv)
-{
-    CommandLineParser parser(argc, argv, keys);
-    parser.about("This sample demonstrates the use of the HoG descriptor.");
-    if (parser.has("help"))
-    {
-        parser.printMessage();
-        return 0;
-    }
-    int camera = parser.get<int>("camera");
-    string file = parser.get<string>("video");
-    if (!parser.check())
-    {
-        parser.printErrors();
-        return 1;
-    }
+	// Create video capturing object
+	// 0 opens default camera, otherwise filename as argument
+	cv::VideoCapture video(0);
 
+	// Check that video is opened
+	if (!video.isOpened()) return -1;
 
+	// For saving the frame
+	cv::Mat frame;
 
-    VideoCapture cap;
-    cap.open(0);
+	// Get video resolution
+	int frameWidth = video.get(cv::CAP_PROP_FRAME_WIDTH);
+	int frameHeigth = video.get(cv::CAP_PROP_FRAME_HEIGHT);
 
-    if (!cap.isOpened())
-    {
-        cerr << "Camera open failed!!" << endl;
-        return -1;
-    }
+	// Create video writer object
+	cv::VideoWriter output("output.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, cv::Size(frameWidth, frameHeigth));
 
-    cout << "Frame width: " << cvRound(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
-    cout << "Frame height: " << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
+	// Create tracker, select region-of-interest (ROI) and initialize the tracker
+	cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
+	video.read(frame);
+	cv::Rect2d trackingBox = cv::selectROI(frame, false);
+	tracker->init(frame, trackingBox);
 
-    cout << "Press 'q' or <ESC> to quit." << endl;
-    cout << "Press <space> to toggle between Default and Daimler detector" << endl;
+	// Loop through available frames
+	while (video.read(frame)) {
 
+		// Update the tracker and draw the rectangle around target if update was successful
+		if (tracker->update(frame, trackingBox)) {
+			cv::rectangle(frame, trackingBox, cv::Scalar(255, 0, 0), 2, 8);
+		}
 
+		// Display the frame
+		cv::imshow("Video feed", frame);
 
-    Detector detector;
+		// Write video frame to output
+		output.write(frame);
 
-    Mat frame;
-    for (;;)
-    {
-        cap >> frame;
-        if (frame.empty())
-        {
-            cout << "Finished reading: empty frame" << endl;
-            break;
-        }
-        int64 t = getTickCount();
-        vector<Rect> found = detector.detect(frame);
-        t = getTickCount() - t;
-        // show the window
-        {
-            ostringstream buf;
-            buf << "Mode: " << detector.modeName() << " ||| "
-                << "FPS: " << fixed << setprecision(1) << (getTickFrequency() / (double)t);
-            putText(frame, buf.str(), Point(10, 30), FONT_HERSHEY_PLAIN, 2.0, Scalar(0, 0, 255), 2, LINE_AA);
-        }
-        for (vector<Rect>::iterator i = found.begin(); i != found.end(); ++i)
-        {
-            Rect &r = *i;
-            detector.adjustRect(r);
-            rectangle(frame, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
-        }
-        imshow("People detector", frame);
-        // interact with user
-        const char key = (char)waitKey(1);
-        if (key == 27 || key == 'q') // ESC
-        {
-            cout << "Exit requested" << endl;
-            break;
-        }
-        else if (key == ' ')
-        {
-            detector.toggleMode();
-        }
-    }
-    return 0;
+		// For breaking the loop
+		if (cv::waitKey(25) >= 0) break;
+
+	} // end while (video.read(frame))
+
+	  // Release video capture and writer
+	output.release();
+	video.release();
+
+	// Destroy all windows
+	cv::destroyAllWindows();
+
+	return 0;
 }
-
-// void camera_in()
-// {
-//     VideoCapture cap;
-//     cap.open(0, CAP_AVFOUNDATION);
-
-//     if (!cap.isOpened())
-//     {
-//         cerr << "Camera open failed!" << endl;
-//         return;
-//     }
-
-//     cout << "Frame width: " << cvRound(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
-//     cout << "Frame height: " << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
-
-//     Mat frame, inversed;
-
-//     while (true)
-//     {
-//         cap >> frame;
-//         if (frame.empty())
-//             break;
-
-//         //inversed = ~frame;
-
-//         imshow("frame", frame);
-//         //imshow("inversed", inversed);
-        
-//         if (waitKey(10) == 27)
-//             break;
-//     }
-//     destroyAllWindows();
-// }
-
-// int main()
-// {
-//     camera_in();
-// }
