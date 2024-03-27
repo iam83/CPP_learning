41b8288cc68b15e8f4445af2911ccc1258bd1f2c
iam83 <iam83iam@gmail.com>
2024-03-27 18:38:06 +0300

    cpp network

---
+++ CPP_Network/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+
+#pragma comment(lib, "ws2_32.dll")
+
+int main(){
+
+
+
+    return 0;
+}
